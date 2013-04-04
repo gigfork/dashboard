348d1b63c9147f85106b81d4c3807c556115f550
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2013-04-04 13:59:15 +0530

    Trying to deploy by curl coz cargo sucks

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,10 +12,14 @@ task :dashboard do
     set :user, "www" 
     set :deploy_to, "/home/#{user}/app/#{application}" # Line 18
     set :use_sudo, false
+    set :tomcat_manager, "manager"
+    set :tomcat_manager_password, "manager"
     namespace :tomcat do
       task :deploy do
-        puts "==================Building and Deploying with Maven======================"
-        run "cd #{deploy_to}/current && mvn clean package && mvn cargo:redeploy"
+        puts "==================Building with Maven======================"
+        run "cd #{deploy_to}/current && mvn clean package"
+        puts "==================Deploying with Manager======================"
+        run "curl --upload-file #{deploy_to}/current/target/#{application}*.war --user #{tomcat_manager}:#{tomcat_manager_password} http://localhost:8080/manager/text/deploy?path=/ROOT"
       end
     end
     after "deploy", "tomcat:deploy"
